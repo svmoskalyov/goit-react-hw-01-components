bba4ba4e29041cd7086a087b3550a3c2a7ef26ac
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-24 21:50:29 +0300

    task01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
+import React from 'react';
+import Profile from "./Profile/Profile";
+import user from "../data/user";
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
   );
 };
+
+export default App;
