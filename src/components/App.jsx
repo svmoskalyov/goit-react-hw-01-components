f3db8a7f3c3da7396c65cb6f88ad5a101a03ed93
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-25 20:33:34 +0300

    task04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 import user from '../data/user';
 import data from '../data/data';
 import friends from '../data/friends';
+import transactions from '../data/transactions';
 
 const App = () => {
   return (
@@ -18,10 +20,9 @@ const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
-
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
