ad1ba2beef6956e1b7f08c52cc5fea5fbb5d5860
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-26 17:50:24 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,35 @@
 import PropTypes from 'prop-types';
+import { Box } from 'components/Box';
+import {
+  Title,
+  StatList,
+  Item,
+  Label,
+  Percentage,
+} from './Statistics.styled';
 
 const Statistics = ({ title, stats }) => {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+    <Box
+      as="section"
+      mx="auto"
+      my={6}
+      width={400}
+      bg="white"
+      borderRadius={4}
+      boxShadow="0 0 4px #a4a2a2"
+    >
+      {title && <Title>{title}</Title>}
 
-      <ul className="stat-list">
+      <StatList>
         {stats.map(({ id, label, percentage }) => (
-          <li key={id} className="item">
-            <span className="label">{label}</span>
-            <span className="percentage">{percentage}%</span>
-          </li>
+          <Item key={id}>
+            <Label>{label}</Label>
+            <Percentage>{percentage}%</Percentage>
+          </Item>
         ))}
-      </ul>
-    </section>
+      </StatList>
+    </Box>
   );
 };
 
