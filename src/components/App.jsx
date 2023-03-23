bbaac55f9202ad79b5a1abd8523e713448dd9ebf
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-03-23 15:44:04 +0200

    creates App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { Box } from './Box';
+import user from 'data/user.json';
+import { Profile } from 'components/Profile/Profile';
+import data from 'data/data.json';
+import { Statistics } from 'components/Statistics/Statistics';
+import friends from 'data/friends.json';
+import { FriendList } from 'components/FriendList/FriendList';
+import transactions from 'data/transactions.json';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box maxWidth="1200px" mx="auto" pt={5}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Box>
   );
 };
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import { Item, Status, Avatar, Name } from './FriendListItem.styled';
+
+export function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <Item>
+      <Status isOnline={isOnline}></Status>
+      <Avatar src={avatar} alt="User avatar" width="48" />
+      <Name>{name}</Name>
+    </Item>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
